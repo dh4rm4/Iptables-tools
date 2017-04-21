cbd9e7bf4338f3787faa7c42fabcedb1353d3a9a
dh4rm4 <dh4rm4@protonmail.com>
2017-04-21 12:38:35 +0200

    add OUTPUT rules

--- init_rules.sh
+++ init_rules.sh
@@ -24,7 +24,6 @@ $MODPROBE iptable_nat
 $MODPROBE ip_conntrack_ftp
 $MODPROBE ip_nat_ftp
 
-
 #####################################
 ###                               ###
 ###        INPUT CHAIN            ###
@@ -47,3 +46,29 @@ $IPTABLES -a INPUT -p icmp --icmp-type echo-request -j ACCEPT
 
 ## DEFAULT INPUT LOG RULE
 $IPTABLES -A INPUT -i ! lo -j LOG --log-prefix "DROP " --log-ip-options --log-tcp-options
+
+#####################################
+###                               ###
+###        OUTPUT CHAIN           ###
+###                               ###
+#####################################
+
+echo "[+] Setting up OUTPUT chain..."
+## STATE TRACKING RULES
+$IPTABLES -A OUTPUT -m state --state INVALID -j LOG --log-prefix "DROP INVALID " --log-ip-options --log-tcp-options
+$IPTABLES -A OUTPUT -m state --state INVALID -j DROP
+$IPTABLES -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+## ACCEPT RULES FOR ALLOWING CONNECTIONS OUT
+$IPTABLES -A OUTPUT -p tcp --dport 21 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 22 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 25 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 43 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 80 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 443 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p tcp --dport 4321 --syn -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p udp --dport 53 -m state --state NEW -j ACCEPT
+$IPTABLES -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
+
+## DEFAULT OUTPUT LOG RULE
+$IPTABLES -A OUTPUT -o ! lo -j LOG --log-prefix "DROP " --log-ip-options --log-tcp-options
